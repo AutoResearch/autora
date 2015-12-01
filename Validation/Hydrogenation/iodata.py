7e87562eba8465bafea148040bb44da449094c49
Roger Guimera <roger.guimera@gmail.com>
2015-12-01 13:26:34 +0100

    Added hydrogenation case study.

---
+++ Validation/Hydrogenation/iodata.py
@@ -0,0 +1,34 @@
+import sys
+import pandas as pd
+
+XVARS = [
+    'v1',
+    'v2',
+    'v3',
+    'v4',
+    'v5',
+    'v6',
+    'v7',
+    'v8',
+    'v9',
+    'v10',
+    'v11',
+    'v12',
+    'v13',
+    'v14',
+    'v15',
+    'v16',
+]
+
+
+def read_data(ylabel='Barrier', xlabels=XVARS, in_fname='H_features.dat'):
+    data = pd.read_csv(in_fname)
+    x = data[xlabels]
+    y = data[ylabel]
+    return data, x, y
+
+if __name__ == '__main__':
+    d, x, y = read_data()
+    
+    print x
+    print y
