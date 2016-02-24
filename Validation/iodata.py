b0f173a21601dada8d0322a837f80af8f5cad2aa
Roger Guimera <roger.guimera@gmail.com>
2016-02-24 17:56:25 +0100

    Added hydrogenation test case to iodata.py.

--- Validation/iodata.py
+++ Validation/iodata.py
@@ -26,14 +26,37 @@ XVARS = {
     'PT_jul',
     'PDO_win',
     ],
+     'Hydrogenation' : [
+            'Barrier',
+            'DE',
+            'NC',
+            'diH',
+            'Tr(R)',
+            'Tr(P)',
+            'DTr',
+            'det(R)',
+            'det(P)',
+            'det(TS)',
+            'RxP',
+            'RxTS',
+            'rank(R)',
+            'rank(P)',
+            'rank(TS)',
+            'SpecRad(R)',
+            'SpecRad(P)',
+            'Type',
+            'ID'
+    ]
 }
 YLABS = {
     'Trepat' : 'Sxx',
     'Ye'     : 'rec',
+    'Hydrogenation':'Barrier',
 }
 FNAMES = {
     'Trepat' : 'cadhesome_protein.csv',
     'Ye'     : 'seymour.csv',
+    'Hydrogenation':'H_features.csv',
 }
 
 def read_data(dset, ylabel=None, xlabels=None, in_fname=None):
@@ -45,7 +68,7 @@ def read_data(dset, ylabel=None, xlabels=None, in_fname=None):
     if in_fname == None:
         in_fname = '%s/data/%s' % (dset, FNAMES[dset])
     # Read 
-    if dset == 'Trepat' or dset == 'Ye':
+    if dset == 'Trepat' or dset == 'Ye' or dset=='Hydrogenation':
         data = pd.read_csv(in_fname)
         x = data[xlabels]
         y = data[ylabel]
