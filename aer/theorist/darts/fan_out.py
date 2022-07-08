1cb9500b68ce429b3396ddedc4e4923909c47547
Sebastian Musslick <sebastian@musslick.de>
2022-07-08 10:33:14 -0400

    commented fan_out class

--- aer/theorist/darts/fan_out.py
+++ aer/theorist/darts/fan_out.py
@@ -3,7 +3,18 @@ import torch.nn as nn
 
 
 class Fan_Out(nn.Module):
-    def __init__(self, num_inputs):
+    """
+    A neural network class that splits a given input vector into separate nodes. Each element of
+    the original input vector is allocated a separate node in a computation graph.
+    """
+
+    def __init__(self, num_inputs: int):
+        """
+        Initialize the Fan Out operation.
+
+        Arguments:
+                num_inputs (int): The number of distinct input nodes to generate
+        """
         super(Fan_Out, self).__init__()
 
         self._num_inputs = num_inputs
@@ -17,6 +28,12 @@ class Fan_Out(nn.Module):
             self.input_output.append(linearConnection)
 
     def forward(self, input):
+        """
+        Forward pass of the Fan Out operation.
+
+        Arguments:
+            input: input vector whose elements are split into separate input nodes
+        """
 
         output = list()
         for i in range(self._num_inputs):
