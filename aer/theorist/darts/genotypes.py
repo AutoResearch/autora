870e1aa6e703d327bf3fba4b36cd866035be906e
Sebastian Musslick <sebastian@musslick.de>
2022-07-08 10:36:33 -0400

    cleaned up genotypes

--- aer/theorist/darts/genotypes.py
+++ aer/theorist/darts/genotypes.py
@@ -2,67 +2,27 @@ from collections import namedtuple
 
 Genotype = namedtuple("Genotype", "normal normal_concat")
 
+# SET OF AVAILABLE PRIMITIVES:
+# "none",
+# "add",
+# "subtract",
+# "linear",
+# "sigmoid",
+# "mult",
+# "exp",
+# "relu",
+# "softplus",
+# "softminus",
+# 'lin_relu',
+# 'lin_sigmoid',
 
-# PRIMITIVES = [
-#     'none',
-#     'relu',
-# ]
 
-
-# PRIMITIVES = [
-#     'none',
-#     'add',
-#     'subtract',
-#     'linear',
-#     'sigmoid',
-#     'exp',
-#     'relu',
-# ]
-
-# PRIMITIVES = [    # success Weber
-#     'none',
-#     'add',
-#     'subtract',
-#     'linear',
-#     'lin_sigmoid',
-#     'mult',
-#     'lin_relu',
-# ]
-
-# PRIMITIVES = [  # partial success weber
-#     'none',
-#     'add',
-#     'subtract',
-#     'linear',
-#     'sigmoid',
-#     'lin_sigmoid',
-#     'mult',
-#     'relu',
-#     'lin_relu',
-# ]
-#
-# PRIMITIVES = [  # STANDARD ANALYSIS
-#     'none',
-#     'add',
-#     'subtract',
-#     'linear',
-#     'sigmoid',
-#     'mult',
-#     'exp',
-#     'relu',
-#     'lin_relu',
-#     'lin_sigmoid',
-# ]
-
-PRIMITIVES = [  # SIMPLIFIED ANALYSIS
+PRIMITIVES = [
     "none",
     "add",
     "subtract",
     "linear",
-    "sigmoid",
+    "lin_sigmoid",
     "mult",
-    "exp",
-    "relu",
-    "softplus",
-    "softminus",
+    "lin_relu",
 ]
