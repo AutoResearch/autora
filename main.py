9a7927c58d1e07eb1a3b7f07cca5fb750ddcc054
TheLemonPig <43144407+TheLemonPig@users.noreply.github.com>
2022-07-24 16:11:00 -0400

    main passes

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-### main file for Bayesian Scientist
+# main file for Bayesian Scientist
 
-### import packages
-import utils
+# import packages
 import sys
-import numpy as np 
-import pandas as pd
 import warnings
-warnings.filterwarnings('ignore')
 
-sys.path.append('./')
-sys.path.append('./Prior/')
-from fit_prior import read_prior_par
+import pandas as pd
 
+import utils
 from parallel import Parallel
+from Prior.fit_prior import read_prior_par
 
-### load data
-'''
+warnings.filterwarnings("ignore")
+
+sys.path.append("./")
+sys.path.append("./Prior/")
+
+
+# load data
+"""
 XLABS = [
     'eff',
     'D_max',
@@ -30,48 +32,48 @@ XLABS = [
     'PT_jun',
     'PT_jul',
     'PDO_win',
-]'''
+]"""
 XLABS = [
-        'S1',
-        'S2',
-         ]
+    "S1",
+    "S2",
+]
 nv = len(XLABS)
 nc = 1
 epochs = 1500
-#raw_data = pd.read_csv('Validation/LogYe/data/seymour.csv')
-#x, y = raw_data[XLABS], np.log(raw_data['rec'])
+# raw_data = pd.read_csv('Validation/LogYe/data/seymour.csv')
+# x, y = raw_data[XLABS], np.log(raw_data['rec'])
 
-raw_data = pd.read_csv('experiment_0_data.csv')
-#x, y = raw_data[XLABS], np.log(raw_data['difference_detected'])
-x, y = raw_data[XLABS], raw_data['difference_detected']
+raw_data = pd.read_csv("experiment_0_data.csv")
+# x, y = raw_data[XLABS], np.log(raw_data['difference_detected'])
+x, y = raw_data[XLABS], raw_data["difference_detected"]
 
-### initialize model
+# initialize model
 # hyper parameters
-prior_par = read_prior_par('./Prior/final_prior_param_sq.named_equations.nv2.np2.2016-09-09 18:49:43.038278.dat')
+prior_par = read_prior_par(
+    "./Prior/final_prior_param_sq.named_equations.nv2.np2.2016-09-09 18:49:43.038278.dat"
+)
 # temperatures
-Ts = [1] + [1.04**k for k in range(1, 20)]
+Ts = [1.0] + [1.04**k for k in range(1, 20)]
 # model
 pms = Parallel(
     Ts,
     variables=XLABS,
-    parameters=['a%d' % i for i in range(nc)],
-    x=x, y=y,
+    parameters=["a%d" % i for i in range(nc)],
+    x=x,
+    y=y,
     prior_par=prior_par,
 )
 
-### run model --- 
-model, model_len, desc_len = utils.run(pms,epochs)
+# run model ---
+model, model_len, desc_len = utils.run(pms, epochs)
 
-### present results
-utils.present_results(pms,
-                      model,
-                      model_len,
-                      desc_len)
+# present results
+utils.present_results(pms, model, model_len, desc_len)
 
-### test predictions
-utils.predict(model,x,y)
+# test predictions
+utils.predict(model, x, y)
 
-'''
+"""
 Places where changes needed to be made in order to incorporate our own priors
 If we plan to give simple priors, then we simply need to give a csv, with the
 operations and their respective probabilities
@@ -83,4 +85,4 @@ need to make a change to the accepted operations at line 22 in mcmc.py
 
 If we plan to make recursive priors, then we will probably need to insert code
 at line 468,1105,1133,1136,1175,1176 in mcmc.py
-'''
+"""
