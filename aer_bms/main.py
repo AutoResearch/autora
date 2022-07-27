1027e82af0e90e6078c7e02f6dd6e24cc2ef3f52
benwandrew <benwallaceandrew@gmail.com>
2022-07-27 11:52:18 -0400

    updated path to experiment_0_data.csv in main.py, switched to new prior to initialize model in main.py

--- aer_bms/main.py
+++ aer_bms/main.py
@@ -5,7 +5,8 @@ import warnings
 
 import pandas as pd
 import utils
-from fit_prior import read_prior_par
+
+# from fit_prior import read_prior_par
 from parallel import Parallel
 
 warnings.filterwarnings("ignore")
@@ -38,37 +39,37 @@ epochs = 300
 # raw_data = pd.read_csv('Validation/LogYe/data/seymour.csv')
 # x, y = raw_data[XLABS], np.log(raw_data['rec'])
 
-raw_data = pd.read_csv("experiment_0_data.csv")
+raw_data = pd.read_csv("aer_bms/experiment_0_data.csv")
 # x, y = raw_data[XLABS], np.log(raw_data['difference_detected'])
 x, y = raw_data[XLABS], raw_data["difference_detected"]
 
 # initialize model
 # hyper parameters
-prior_par = read_prior_par(
-    "./Prior/final_prior_param_sq.named_equations.nv2.np2.2016-09-09 18:49:43.038278.dat"
-)
-"""
+# prior_par = read_prior_par(
+#    "./Prior/final_prior_param_sq.named_equations.nv2.np2.2016-09-09 18:49:43.038278.dat"
+# )
+
 prior_par = {
-        "Nopi_/": 5.912205942815285,
-        "Nopi_cosh": 8.12720511103694,
-        "Nopi_-": 3.350846072163632,
-        "Nopi_sin": 5.965917796154835,
-        "Nopi_tan": 8.127427922862411,
-        "Nopi_tanh": 7.799259068142255,
-        "Nopi_**": 6.4734429542245495,
-        "Nopi_pow2": 3.3017352779079734,
-        "Nopi_pow3": 5.9907496760026175,
-        "Nopi_exp": 4.768665265735502,
-        "Nopi_log": 4.745957377206544,
-        "Nopi_sqrt": 4.760686909134266,
-        "Nopi_cos": 5.452564657261127,
-        "Nopi_sinh": 7.955723540761046,
-        "Nopi_abs": 6.333544134938385,
-        "Nopi_+": 5.808163661224514,
-        "Nopi_*": 5.002213595420244,
-        "Nopi_fac": 10.0,
-        "Nopi2_*": 1.0,
-    }"""
+    "Nopi_/": 5.912205942815285,
+    "Nopi_cosh": 8.12720511103694,
+    "Nopi_-": 3.350846072163632,
+    "Nopi_sin": 5.965917796154835,
+    "Nopi_tan": 8.127427922862411,
+    "Nopi_tanh": 7.799259068142255,
+    "Nopi_**": 6.4734429542245495,
+    "Nopi_pow2": 3.3017352779079734,
+    "Nopi_pow3": 5.9907496760026175,
+    "Nopi_exp": 4.768665265735502,
+    "Nopi_log": 4.745957377206544,
+    "Nopi_sqrt": 4.760686909134266,
+    "Nopi_cos": 5.452564657261127,
+    "Nopi_sinh": 7.955723540761046,
+    "Nopi_abs": 6.333544134938385,
+    "Nopi_+": 5.808163661224514,
+    "Nopi_*": 5.002213595420244,
+    "Nopi_fac": 10.0,
+    "Nopi2_*": 1.0,
+}
 # Prior.fit_prior()
 
 
