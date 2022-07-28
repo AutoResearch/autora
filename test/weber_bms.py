8a4df4affdb97a3a216252d9de43dfd4966f15cd
TheLemonPig <43144407+TheLemonPig@users.noreply.github.com>
2022-07-28 16:12:28 -0400

    Update test/weber_bms.py
    Co-authored-by: John Gerrard Holland <john_holland1@brown.edu>

--- test/weber_bms.py
+++ test/weber_bms.py
@@ -38,7 +38,7 @@ epochs = 300
 # raw_data = pd.read_csv('Validation/LogYe/data/seymour.csv')
 # x, y = raw_data[XLABS], np.log(raw_data['rec'])
 
-raw_data = pd.read_csv("test/experiment_0_data.csv")
+raw_data = pd.read_csv("./experiment_0_data.csv")
 # x, y = raw_data[XLABS], np.log(raw_data['difference_detected'])
 x, y = raw_data[XLABS], raw_data["difference_detected"]
 
