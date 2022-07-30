c2847d47ebbe7f932acafb2669031599cd6f6d8a
Sebastian Musslick <sebastian@musslick.de>
2022-07-29 20:22:23 -0400

    replaced deprecated np.asscalar() with np.nparray.item()

--- aer/theorist/darts/model_search.py
+++ aer/theorist/darts/model_search.py
@@ -564,11 +564,9 @@ class Network(nn.Module):
             maxIdx = np.where(values == max(values))
 
             tmp_param_list = list()
-            if Network.isiterable(
-                op._ops[np.asscalar(maxIdx[0])]
-            ):  # Zero is not iterable
+            if Network.isiterable(op._ops[maxIdx[0].item(0)]):  # Zero is not iterable
 
-                for subop in op._ops[np.asscalar(maxIdx[0])]:
+                for subop in op._ops[maxIdx[0].item(0)]:
 
                     for parameter in subop.parameters():
                         tmp_param_list.append(parameter.data.numpy().squeeze())
@@ -581,9 +579,7 @@ class Network(nn.Module):
                     "Edge ("
                     + str(idx)
                     + "): "
-                    + get_operation_label(
-                        PRIMITIVES[np.asscalar(maxIdx[0])], tmp_param_list
-                    )
+                    + get_operation_label(PRIMITIVES[maxIdx[0].item(0)], tmp_param_list)
                 )
             param_list.append(tmp_param_list)
 
