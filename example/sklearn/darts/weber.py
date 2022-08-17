f5e6b6748dd6bc450469be53fd26ac581b0be8da
John Gerrard Holland <john_holland1@brown.edu>
2022-08-17 11:01:36 -0400

    fix: remove excess white-lines

--- example/sklearn/darts/weber.py
+++ example/sklearn/darts/weber.py
@@ -43,12 +43,9 @@ def show_results_complete(
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
         ax.scatter(data_["S1"], data["S2"], data["difference_detected"])
-
         if estimator is not None:
             xs, ys = np.mgrid[0:5:0.2, 0:5:0.2]  # type: ignore
-
             zs = estimator.predict(np.column_stack((xs.ravel(), ys.ravel())))
-
             ax.plot_surface(xs, ys, zs.reshape(xs.shape), alpha=0.5)
 
     if label is not None:
@@ -61,21 +58,15 @@ def show_results_complete(
 
 
 # %% Load the data
-
-
 data = pd.read_csv("./weber_data.csv")  # reads from resource root data
 show_results = partial(show_results_complete, data_=data, projection="3d")
 show_results(label="input data")
-
 X = data[["S1", "S2"]]
 y = data["difference_detected"]
 
-
 # %% Fit first using a super-simple linear regression
-
 first_order_linear_estimator = LinearRegression()
 first_order_linear_estimator.fit(X, y)
-
 show_results(estimator=first_order_linear_estimator, label="1st order linear")
 
 # %% Fit using a 0-3 order polynomial, getting the best fit for the data.
@@ -84,7 +75,6 @@ polynomial_estimator = GridSearchCV(
     param_grid=dict(polynomialfeatures__degree=range(4)),
 )
 polynomial_estimator.fit(X, y)
-
 show_results(estimator=polynomial_estimator, label="[0th-3rd]-order linear")
 
 # %% Fit using DARTS with parameters we know work
@@ -97,9 +87,7 @@ darts_estimator_tuned = DARTSRegressor(
     darts_type="original",
     num_graph_nodes=2,
 )
-
 darts_estimator_tuned.fit(X, y)
-
 show_results(
     estimator=darts_estimator_tuned, label="pre-tuned Original-DARTS Regressor"
 )
@@ -115,16 +103,11 @@ darts_estimator_tuned = DARTSRegressor(
     darts_type="fair",
     num_graph_nodes=2,
 )
-
 darts_estimator_tuned.fit(X, y)
-
 show_results(estimator=darts_estimator_tuned, label="pre-tuned Fair-DARTS Regressor")
 darts_estimator_tuned.visualize_model().view()
 
-
 # %%
-
-
 darts_estimator_tuned.resample_model(sampling_strategy="sample")
 show_results(estimator=darts_estimator_tuned, label="resampled DARTSRegressor")
 darts_estimator_tuned.visualize_model().view()
@@ -146,14 +129,11 @@ darts_estimator = GridSearchCV(
     n_jobs=5,
 )
 darts_estimator.fit(X, y)
-
 print(darts_estimator.best_params_)
 show_results(estimator=darts_estimator.best_estimator_, label="DARTSRegressor")
 darts_estimator.best_estimator_[1].visualize_model().view()
 
-
 # %% Fit using Multilayer-Perceptron
 mlp_estimator = MLPRegressor()
 mlp_estimator.fit(X, y)
-
 show_results(estimator=mlp_estimator, label="Multilayer-Perceptron")
