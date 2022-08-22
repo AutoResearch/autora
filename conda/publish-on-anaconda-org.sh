39f0a1ef13faa5b9612d3fe33801aa297b9b22cc
John Gerrard Holland <john_holland1@brown.edu>
2022-08-22 12:11:13 -0400

    feat: add add better Token handling for Conda

--- conda/publish-on-anaconda-org.sh
+++ conda/publish-on-anaconda-org.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+
+die() {
+  local message="$@"
+  >&2 echo -e "${message}"
+  exit 1
+}
+
+source env.sh  || die ".env file not found. Run: echo ANACONDA_AUTO_RESEARCH_TOKEN=\"\$(anaconda auth --create --name AutoResearchToken --org AutoResearch)\" > .env"
 conda config --set anaconda_upload yes
-TOKEN=$(anaconda auth --create --name AutoResearchToken --org AutoResearch)
-conda build . -c pytorch --output-folder dist/ --token "$TOKEN"
+conda build . -c pytorch --output-folder dist/ --token "$ANACONDA_AUTO_RESEARCH_TOKEN"
