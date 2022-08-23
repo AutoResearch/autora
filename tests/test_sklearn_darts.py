2b6a3483a64b506067db188c2bf48fc40a48db13
John Gerrard Holland <john_holland1@brown.edu>
2022-08-23 09:55:13 -0400

    feat: add tests of primitive recovery for darts

--- tests/test_sklearn_darts.py
+++ tests/test_sklearn_darts.py
@@ -1,3 +1,5 @@
+from typing import Callable, Sequence
+
 import numpy as np
 import pytest
 from sklearn.model_selection import GridSearchCV, train_test_split
@@ -17,6 +19,97 @@ def generate_constant_data(const: float = 0.5, num: int = 1000):
     X = np.expand_dims(np.linspace(start=0, stop=1, num=num), 1)
     y = const * np.ones(num)
     return X, y, const
+
+
+def transform_through_primitive_none(x: np.ndarray) -> np.ndarray:
+    return x * 0.0
+
+
+def transform_through_primitive_add(x: np.ndarray) -> np.ndarray:
+    return x
+
+
+def transform_through_primitive_subtract(x: np.ndarray) -> np.ndarray:
+    return -x
+
+
+def transform_through_primitive_relu(x: np.ndarray):
+    y = x.copy()
+    y[x < 0] = 0.0
+    return y
+
+
+def get_primitive_from_single_node_model(model):
+    primitive = model[0].primitives[np.argmax(model[0].max_alphas_normal()).numpy()]
+    return primitive
+
+
+def run_test_primitive_fitting(
+    X: np.ndarray,
+    transformer: Callable,
+    expected_primitive: str,
+    primitives: Sequence[str] = PRIMITIVES,
+):
+    y = transformer(X)
+    regressor = DARTSRegressor(
+        num_graph_nodes=1,
+        param_updates_per_epoch=100,
+        max_epochs=100,
+        arch_updates_per_epoch=100,
+        primitives=primitives,
+    )
+    regressor.fit(X, y)
+    assert get_primitive_from_single_node_model(regressor.model_) == expected_primitive
+
+
+def test_primitive_fitting_restricted_none():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(
+        X,
+        transform_through_primitive_none,
+        "none",
+        primitives=["none", "add", "subtract"],
+    )
+
+
+def test_primitive_fitting_restricted_add():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(
+        X,
+        transform_through_primitive_add,
+        "add",
+        primitives=["none", "add", "subtract"],
+    )
+
+
+def test_primitive_fitting_restricted_subtract():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(
+        X,
+        transform_through_primitive_subtract,
+        "subtract",
+        primitives=["none", "add", "subtract"],
+    )
+
+
+def test_primitive_fitting_none():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(X, transform_through_primitive_none, "none")
+
+
+def test_primitive_fitting_lin_relu():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(X, transform_through_primitive_relu, "lin_relu")
+
+
+def test_primitive_fitting_add():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(X, transform_through_primitive_add, "add")
+
+
+def test_primitive_fitting_subtract():
+    X = np.expand_dims(np.linspace(start=-1, stop=1, num=100), 1)
+    run_test_primitive_fitting(X, transform_through_primitive_subtract, "subtract")
 
 
 def test_constant_model():
