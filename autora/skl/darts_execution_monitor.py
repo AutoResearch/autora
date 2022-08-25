2a0d90aaac951c7c545e2d22afbf149d6e4bae8b
Sebastian Musslick <sebastian@musslick.de>
2022-08-25 11:46:20 -0400

    added plot labels

--- autora/skl/darts_execution_monitor.py
+++ autora/skl/darts_execution_monitor.py
@@ -25,14 +25,24 @@ class BasicExecutionMonitor:
         loss_fig, loss_ax = plt.subplots(1, 1)
         loss_ax.plot(self.loss_history)
 
+        loss_ax.set_ylabel("Loss", fontsize=14)
+        loss_ax.set_xlabel("Epoch", fontsize=14)
+        loss_ax.set_title("Training Loss")
+
         arch_weight_history_array = np.vstack(self.arch_weight_history)
         num_epochs, num_edges, num_primitives = arch_weight_history_array.shape
 
         subplots_per_side = int(np.ceil(np.sqrt(num_edges)))
 
         arch_fig, arch_axes = plt.subplots(
-            subplots_per_side, subplots_per_side, sharex=True, sharey=True
+            subplots_per_side,
+            subplots_per_side,
+            sharex=True,
+            sharey=True,
+            figsize=(10, 10),
         )
+
+        arch_fig.suptitle("Architecture Weights", fontsize=10)
 
         for (edge_i, ax) in zip(range(num_edges), arch_axes.reshape(-1)):
             for primitive_i in range(num_primitives):
@@ -42,7 +52,13 @@ class BasicExecutionMonitor:
                     label=f"{self.primitives[primitive_i]}",
                 )
 
-            ax.legend(loc="upper center")
+            ax.set_title("k{}".format(edge_i), fontsize=8)
+
+            # there is no need to have the legend for each subplot
+            if edge_i == 0:
+                ax.legend(loc="upper center")
+                ax.set_ylabel("Edge Weights", fontsize=8)
+                ax.set_xlabel("Epoch", fontsize=8)
 
         return SimpleNamespace(
             loss_fig=loss_fig,
