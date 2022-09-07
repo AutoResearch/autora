557138da45cb635d21ea99990377b3db4e788435
TheLemonPig <43144407+TheLemonPig@users.noreply.github.com>
2022-09-07 15:27:29 -0400

    adding initial bms unit test file

---
+++ tests/test_sklearn_bms.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from aer_bms.skl.bms import BMS
+
+
+def generate_noisy_constant_data(
+    const: float = 0.5, epsilon: float = 0.01, num: int = 1000, seed: int = 42
+):
+    X = np.expand_dims(np.linspace(start=0, stop=1, num=num), 1)
+    y = np.random.default_rng(seed).normal(loc=const, scale=epsilon, size=num)
+    return X, y, const, epsilon
+
+
+def generate_noisy_sin_data(
+    const: float = 0.5, epsilon: float = 0.01, num: int = 1000, seed: int = 42
+):
+    X = np.expand_dims(np.linspace(start=0, stop=1, num=num), 1)
+    y = np.sin(np.random.default_rng(seed).normal(loc=const, scale=epsilon, size=num))
+    return X, y, const, epsilon
+
+
+def test_constant_model():
+    X, y, const, epsilon = generate_noisy_constant_data()
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    estimator = BMS()
+    assert estimator is not None
+    estimator.fit(X_train, y_train)
+    print(estimator.model_)
+
+
+def test_sin_model():
+    X, y, const, epsilon = generate_noisy_sin_data()
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    estimator = BMS()
+    assert estimator is not None
+    estimator.fit(X_train, y_train)
+    print(estimator.model_)
+
+
+if __name__ == "__main__":
+    test_constant_model()
+    test_sin_model()
