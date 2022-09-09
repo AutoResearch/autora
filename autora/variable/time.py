1a41d1dd6c6c15044350139b2da5a7e0e5a5224e
John Gerrard Holland <john_holland1@brown.edu>
2022-09-09 13:25:12 -0400

    chore: remove broken operations from time and tinkerforge.py

--- autora/variable/time.py
+++ autora/variable/time.py
@@ -1,6 +1,6 @@
 import time
 
-from autora.variable import DV, IV, register_dv_label, register_iv_label
+from autora.variable import DV, IV
 
 
 class VTime:
@@ -101,8 +101,3 @@ class DVTime(DV, VTime):
         """
         value = time.time() - self._t0
         self.set_value(value)
-
-
-register_dv_label(**{"time_DV": (DVTime, "Time", "", "time_DV", "s", 0, (0, 3600))})
-
-register_iv_label(**{"time_IV": (IVTime, "Time", "", "time_IV", "s", 1, (0, 3600))})
