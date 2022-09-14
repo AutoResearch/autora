e3f14f4534c5a1733a5875978086bec4d7463adc
George Dang <george_dang@brown.edu>
2022-09-14 14:33:32 -0400

    Added comments and edits for clarity. Added pycharm .idea/ to gitignore.

--- tests/test_cycle.py
+++ tests/test_cycle.py
@@ -13,12 +13,22 @@ from autora.variable import Variable, VariableCollection
 
 
 def test_cycle():
+    """
+    This test prototypes the design and calling of the closed cycle system starting from
+    different modules of the cycle (>> Experiment Runner > Theorist > Experimentalist >>).
+    Different starting points of the cycle are invoked by handlers with the prefix `start_<module>`.
+    This prototype uses simplistic representations of the theorist, experimentalist, and experiment
+    runner.
 
-    x1 = np.linspace(0, 1, 100)
+    theorist: Generates a constant theory of  y = x + 1
+    experimentalist: Generates new independent variables (x') to experiment from a uniform random
+                     selection of values from a defined range.
+    experiment_runner: Represents data gained from an experiment that perfectly matches the theory
+                       y = x' + 1
 
-    def experiment_runner(x):
-        return x + 1
+    """
 
+    # Define basic versions of
     def dummy_theorist(data, metadata, search_space):
         def theory(x):
             return x + 1
@@ -31,15 +41,19 @@ def test_cycle():
         x_prime = np.random.uniform(low, high, 10)
         return x_prime
 
+    def dummy_experiment_runner(x_prime):
+        return x_prime + 1
+
+    #  Define parameters for run
+    x1 = np.linspace(0, 1, 100)
     metadata = VariableCollection(
         independent_variables=[Variable(name="x1", value_range=(-5, 5))],
         dependent_variables=[Variable(name="y", value_range=(-10, 10))],
     )
-
     parameters = dict(
         cycle_count=0,
         max_cycle_count=10,
-        experiment_runner=experiment_runner,
+        experiment_runner=dummy_experiment_runner,
         independent_variable_values=x1,
         data=DataSetCollection([]),
         theorist=dummy_theorist,
@@ -48,15 +62,21 @@ def test_cycle():
         experimentalist=dummy_experimentalist,
         theory=None,
     )
+
+    # Test invoking the cycle at different start points using start handlers
+    # Run from experiment runner
     experiment_runner_results = start_experiment_runner(**parameters)
     print(experiment_runner_results)
+    # # Run starting from theorist
     theorist_results = start_theorist(**parameters)
     print(theorist_results)
+    # # Run starting from experimentalist
     experimentalist_results = start_experimentalist(**parameters)
     print(experimentalist_results)
 
+    # Runs using run statement
     experiment_runner_results_run = run(
-        starting_point="experiment_runner", **parameters
+        starting_point="dummy_experiment_runner", **parameters
     )
     print(experiment_runner_results_run)
 
