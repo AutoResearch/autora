eccf82710728e0739adb1a7846f73f84ad1e9306
TheLemonPig <joshua_hewson@brown.edu>
2022-10-06 14:38:02 -0400

    priors moved to prior.py and called from there

--- aer_bms/skl/bms.py
+++ aer_bms/skl/bms.py
@@ -8,31 +8,13 @@ from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 
 from aer_bms import Parallel, utils
 from aer_bms.mcmc import Tree
+from aer_bms.prior import get_prior
 
 _logger = logging.getLogger(__name__)
+
 # hyperparameters for BMS
 # 1) Priors for MCMC
-PRIORS = {
-    "Nopi_/": 5.912205942815285,
-    "Nopi_cosh": 8.12720511103694,
-    "Nopi_-": 3.350846072163632,
-    "Nopi_sin": 5.965917796154835,
-    "Nopi_tan": 8.127427922862411,
-    "Nopi_tanh": 7.799259068142255,
-    "Nopi_**": 6.4734429542245495,
-    "Nopi_pow2": 3.3017352779079734,
-    "Nopi_pow3": 5.9907496760026175,
-    "Nopi_exp": 4.768665265735502,
-    "Nopi_log": 4.745957377206544,
-    "Nopi_sqrt": 4.760686909134266,
-    "Nopi_cos": 5.452564657261127,
-    "Nopi_sinh": 7.955723540761046,
-    "Nopi_abs": 6.333544134938385,
-    "Nopi_+": 5.808163661224514,
-    "Nopi_*": 5.002213595420244,
-    "Nopi_fac": 10.0,
-    "Nopi2_*": 1.0,
-}
+PRIORS = get_prior()
 
 # 2) Temperatures for parallel tempering
 TEMPERATURES = [1.0] + [1.04**k for k in range(1, 20)]
