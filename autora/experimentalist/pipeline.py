c61bf5c752cad8e391d9ebbe27476ffbeb755b09
George Dang <53052793+gtdang@users.noreply.github.com>
2022-11-14 17:17:52 -0500

    Updated experimentalist pipeline to work with generators.

--- autora/experimentalist/pipeline.py
+++ autora/experimentalist/pipeline.py
@@ -23,11 +23,33 @@ class Pipeline:
     def __init__(self, pool: Pool, *pipes: Pipe):
         self.pool = pool
         self.pipes = pipes
+        self.results: List[ExperimentalSequence] = []
 
     def __call__(self) -> ExperimentalSequence:
-        results: List[ExperimentalSequence] = []
-        results.append(self.pool())
-        for pipe in self.pipes:
-            results.append(pipe(results[-1]))
+        self.results = []
+        # Create pool
+        if callable(self.pool):
+            self.results.append(self.pool())
+        else:
+            self.results.append(self.pool)
 
-        return results[-1]
+        # Run filters
+        for pipe in self.pipes:
+            self.results.append(pipe(self.results[-1]))
+
+        return self.results[-1]
+
+    # May be more intuitive to run the pipeline with a named method. Similar to skl.
+    def run(self):
+        self.results = []
+        # Create pool
+        if callable(self.pool):
+            self.results.append(self.pool())
+        else:
+            self.results.append(self.pool)
+
+        # Run filters
+        for pipe in self.pipes:
+            self.results.append(pipe(self.results[-1]))
+
+        return self.results[-1]
