4154b1545a862926575b060b4e8572f1d36e667c
John Gerrard Holland <john_holland1@brown.edu>
2022-11-17 16:51:15 -0500

    chore: remove unused shell function

--- conda/publish-on-anaconda-org.sh
+++ conda/publish-on-anaconda-org.sh
@@ -1,9 +1,4 @@
 #!/bin/zsh
-die() {
-  local message="$@"
-  >&2 echo "${message}"
-  exit 1
-}
 
 conda config --set anaconda_upload yes
 TOKEN=$(anaconda auth --create --org AutoResearch --name "$(uname -n)-$(date +%s)" --max-age 3600  || die "Token could not be created.")
