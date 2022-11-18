0360a229a61760718d056d7ead61b7a5f779a7a4
John Gerrard Holland <john_holland1@brown.edu>
2022-11-18 14:11:44 -0500

    chore: remove broken scopes argument

--- conda/publish-on-anaconda-org.sh
+++ conda/publish-on-anaconda-org.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 conda config --set anaconda_upload yes
-TOKEN=$(anaconda auth --create --org AutoResearch --name "$(uname -n)-$(date +%s)" --max-age 3600 --scopes anaconda_upload)
+TOKEN=$(anaconda auth --create --org AutoResearch --name "$(uname -n)-$(date +%s)" --max-age 3600)
 conda build ./recipe -c pytorch --output-folder dist/ --token "$TOKEN"
