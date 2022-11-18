46ce3a5f2a05b90f9917e39b37abffe8d42a863c
John Gerrard Holland <john_holland1@brown.edu>
2022-11-18 13:18:32 -0500

    chore: remove die in TOKEN generator

--- publish-on-anaconda-org.sh
+++ publish-on-anaconda-org.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 conda config --set anaconda_upload yes
-TOKEN=$(anaconda auth --create --org AutoResearch --name "$(uname -n)-$(date +%s)" --max-age 3600  || die "Token could not be created.")
-conda build . -c pytorch --output-folder dist/ --token "$TOKEN"
+TOKEN=$(anaconda auth --create --org AutoResearch --name "$(uname -n)-$(date +%s)" --max-age 3600)
+conda build . -c pytorch --output-folder dist/anaconda --token "$TOKEN"
