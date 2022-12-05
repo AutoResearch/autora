375a5d791f90790b1a448a663f54f76d970dbecb
TheLemonPig <joshua_hewson@brown.edu>
2022-12-05 16:24:38 -0500

    add variable to BMSRegressor to store multiple models

--- autora/skl/bms.py
+++ autora/skl/bms.py
@@ -70,11 +70,14 @@ class BMSRegressor(BaseEstimator, RegressorMixin):
         self.X_: Optional[np.ndarray] = None
         self.y_: Optional[np.ndarray] = None
         self.model_: Tree = Tree()
+        self.models_: List[Tree] = [Tree()]
         self.loss_: float = np.inf
         self.cache_: List = []
         self.variables: List = []
 
-    def fit(self, X: np.ndarray, y: np.ndarray, num_param: int = 1) -> BMSRegressor:
+    def fit(
+        self, X: np.ndarray, y: np.ndarray, num_param: int = 1, out: int = 1
+    ) -> BMSRegressor:
         """
         Runs the optimization for a given set of `X`s and `y`s.
 
@@ -110,6 +113,7 @@ class BMSRegressor(BaseEstimator, RegressorMixin):
             prior_par=self.prior_par,
         )
         self.model_, self.loss_, self.cache_ = utils.run(self.pms, self.epochs)
+        self.models_ = list(self.pms.trees.values())[:out]
 
         _logger.info("BMS fitting finished")
         self.X_, self.y_ = X, y
