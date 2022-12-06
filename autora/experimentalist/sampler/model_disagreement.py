28fc04052bfc423f4875e2205faef729a3348aa5
Sebastian Musslick <sebastian@musslick.de>
2022-12-06 16:22:47 -0500

    using itertools instead of double for loop

--- autora/experimentalist/sampler/model_disagreement.py
+++ autora/experimentalist/sampler/model_disagreement.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Iterable, List
 
 import numpy as np
@@ -26,40 +27,35 @@ def model_disagreement_sampler(X: np.array, models: List, num_samples: int = 1):
     model_disagreement = list()
 
     # collect diagreements for each model apir
-    for idx_A, model_A in enumerate(models):
-        for idx_B, model_B in enumerate(models):
+    for model_A, model_B in itertools.combinations(models, 2):
 
-            # don't compare the model with itself
-            if idx_A <= idx_B:
-                continue
+        # determine the prediction method
+        model_A_predict = getattr(model_A, "predict_proba", None)
+        if callable(model_A_predict) is False:
+            model_A_predict = getattr(model_A, "predict", None)
+            model_B_predict = getattr(model_B, "predict", None)
+        else:
+            model_B_predict = getattr(model_B, "predict_proba", None)
+            if callable(model_B_predict) is False:
+                raise Exception("Models must have the same prediction method.")
 
-            # determine the prediction method
-            model_A_predict = getattr(model_A, "predict_proba", None)
-            if callable(model_A_predict) is False:
-                model_A_predict = getattr(model_A, "predict", None)
-                model_B_predict = getattr(model_B, "predict", None)
-            else:
-                model_B_predict = getattr(model_B, "predict_proba", None)
-                if callable(model_B_predict) is False:
-                    raise Exception("Models must have the same prediction method.")
+        if model_A_predict is None or model_B_predict is None:
+            raise Exception("Model must have `predict` or `predict_proba` method.")
 
-            if model_A_predict is None or model_B_predict is None:
-                raise Exception("Model must have `predict` or `predict_proba` method.")
+        # get predictions from both models
+        Y_A = model_A_predict(X_predict)
+        Y_B = model_B_predict(X_predict)
 
-            # get predictions from both models
-            Y_A = model_A_predict(X_predict)
-            Y_B = model_B_predict(X_predict)
+        if Y_A.shape != Y_B.shape:
+            raise Exception("Models must have same output shape.")
 
-            if Y_A.shape != Y_B.shape:
-                raise Exception("Models must have same output shape.")
+        # determine the disagreement between the two models in terms of mean-squared error
+        if len(Y_A.shape) == 1:
+            disagreement = (Y_A - Y_B) ** 2
+        else:
+            disagreement = np.mean((Y_A - Y_B) ** 2, axis=1)
 
-            # determine the disagreement between the two models in terms of mean-squared error
-            if len(Y_A.shape) == 1:
-                disagreement = (Y_A - Y_B) ** 2
-            else:
-                disagreement = np.mean((Y_A - Y_B) ** 2, axis=1)
-
-            model_disagreement.append(disagreement)
+        model_disagreement.append(disagreement)
 
     if len(model_disagreement) == 0:
         raise Exception("No models to compare.")
