253a6968b90802abfa226336ad6bb1222ed1ca57
George Dang <53052793+gtdang@users.noreply.github.com>
2022-12-21 16:00:59 -0500

    Add isort skip in autora.cycle __init__ file.

--- autora/cycle/__init__.py
+++ autora/cycle/__init__.py
@@ -1,2 +1,3 @@
-from .plot_utils import plot_results_panel_2d, plot_results_panel_3d
 from .simple import SimpleCycle as Cycle
+
+from .plot_utils import plot_results_panel_2d, plot_results_panel_3d  # isort:skip
