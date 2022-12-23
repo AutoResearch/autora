2a659649c2807dcfab724817ed0770513bb858b9
George Dang <53052793+gtdang@users.noreply.github.com>
2022-12-23 10:21:34 -0500

    Update cycle __init__

--- autora/cycle/__init__.py
+++ autora/cycle/__init__.py
@@ -1,3 +1,9 @@
 from .simple import SimpleCycle as Cycle
 
-from .plot_utils import plot_results_panel_2d, plot_results_panel_3d  # isort:skip
+from .plot_utils import (  # isort:skip
+    cycle_default_score,
+    cycle_specified_score,
+    plot_cycle_score,
+    plot_results_panel_2d,
+    plot_results_panel_3d,
+)
