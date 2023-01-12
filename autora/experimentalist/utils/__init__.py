704d6a67596c0e21dba032f949211f4e4c7d2d3a
John Gerrard Holland <john_holland1@brown.edu>
2023-01-12 22:47:41 +0100

    refactor: get rid of ArrayPipelineWrapper class – it's way too complicated and doesn't give us much benefit.

--- autora/experimentalist/utils/__init__.py
+++ autora/experimentalist/utils/__init__.py
@@ -1,150 +1,14 @@
 from __future__ import annotations
 
 import collections
-from functools import partial
-from typing import Any, Dict, List, Literal, Optional, Sequence, Union
+from typing import Union
 
 import numpy as np
 
-from autora.experimentalist.pipeline import Pipeline, _ExperimentalSequence, _StepType
 
-
-class ArrayPipelineWrapper(Pipeline):
+def sequence_to_array(iterable):
     """
-    A pipeline which uses arrays as its internal representation of data, and accepts/ouputs
-    sequences.
-
-    This is useful when Pipes which expect finite arrays as input and produce arrays as output,
-    rather than a (potentially unbounded) iterable of experimental conditions.
-
-    This type of pipeline only accepts bounded Sequences as inputs.
-
-    Examples:
-        The empty ArrayPipelineWrapper just returns the input sequence as arrays
-        >>> p0 = ArrayPipelineWrapper([])
-        >>> list(p0.run(zip(range(5), range(5,15))))
-        [(0, 5), (1, 6), (2, 7), (3, 8), (4, 9)]
-
-        The intermediate pipe steps can use Numpy functionality by default:
-        >>> p1 = ArrayPipelineWrapper([
-        ...     ("mask", lambda cs: cs[[True, False, True, True, False]])
-        ... ])
-        >>> list(p1.run(range(5)))
-        [(0,), (2,), (3,)]
-
-        >>> def echo(x):
-        ...     print(f"within the pipeline, x is a numpy.array: {x=}")
-        ...     return x
-        >>> p2 = ArrayPipelineWrapper([
-        ...     ("mask", lambda x: x[[True, False, True, True, False]]),
-        ...     ("echo", echo)
-        ... ], array_type="numpy.array")
-        >>> p2_output = p2.run(range(5))  # doctest: +NORMALIZE_WHITESPACE
-        within the pipeline, x is a numpy.array: x=array([[0], [2], [3]])
-        >>> list(p2_output)
-        [array([0]), array([2]), array([3])]
-
-        You can also use this with a pooler which produces arrays:
-        >>> p3 = ArrayPipelineWrapper([
-        ...     ("pool", np.arange(10).reshape(-1, 2)),
-        ...     ("mask", lambda cs: cs[[True, False, True, True, False]])
-        ... ])
-        >>> list(p3.run())
-        [array([0, 1]), array([4, 5]), array([6, 7])]
-
-    """
-
-    def __init__(
-        self,
-        steps: Optional[Sequence[_StepType]] = None,
-        params: Optional[Dict[str, Any]] = None,
-        array_type: Literal["numpy.array", "numpy.rec.array"] = "numpy.rec.array",
-    ):
-        steps_with_wrappers: List[_StepType] = []
-
-        if (
-            steps is None
-            or len(steps) == 0
-            or not isinstance(steps[0][1], (np.ndarray, np.recarray))
-        ):
-            steps_with_wrappers.extend(
-                [("sequence_to_array", partial(sequence_to_array, type=array_type))]
-            )
-        else:
-            pass  # we don't need to convert the input – we just assume the first step is a pool
-            # which produces a valid array
-
-        if steps is None:
-            pass
-        else:
-            steps_with_wrappers.extend(steps)
-
-        steps_with_wrappers.extend([("array_to_sequence", partial(array_to_sequence))])
-
-        super().__init__(steps=steps_with_wrappers, params=params)
-        return
-
-
-def sequence_to_array(
-    input: _ExperimentalSequence, array_type: Literal["numpy.array", "numpy.rec.array"]
-):
-    """
-    Converts a finite sequence of experimental conditions into a 2D array.
-
-    See also: [array_to_sequence][autora.experimentalist.utils.array_to_sequence]
-
-    Examples:
-
-        A simple range object can be converted into an array of dimension 2:
-        >>> sequence_to_array(range(5),array_type="numpy.array") # doctest: +NORMALIZE_WHITESPACE
-        array([[0], [1], [2], [3], [4]])
-
-        An alternative representation is the record array, also of dimension 2:
-        >>> sequence_to_array(range(5),array_type="numpy.rec.array"
-        ...     ) # doctest: +NORMALIZE_WHITESPACE
-        rec.array([(0,), (1,), (2,), (3,), (4,)], dtype=[('f0', '<i8')])
-
-        For mixed datatypes, it is sensible to use a record array:
-        >>> sequence_to_array(zip(range(5), "abcde"),array_type="numpy.rec.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        rec.array([(0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e')],
-            dtype=[('f0', '<i8'), ('f1', '<U1')])
-
-        ... otherwise, the highest-level type common to all the inputs will be used:
-        >>> sequence_to_array(zip(range(5), "abcde"),array_type="numpy.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        array([['0', 'a'], ['1', 'b'], ['2', 'c'], ['3', 'd'], ['4', 'e']],  dtype='<U21')
-
-        Single strings are broken into characters:
-        >>> sequence_to_array("abcde",array_type="numpy.rec.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        rec.array([('a',), ('b',), ('c',), ('d',), ('e',)], dtype=[('f0', '<U1')])
-
-        >>> sequence_to_array("abcde",array_type="numpy.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        array([['a'], ['b'], ['c'], ['d'], ['e']], dtype='<U1')
-
-        Multiple strings are treated as individual entries:
-        >>> sequence_to_array(["abc", "de"],array_type="numpy.rec.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        rec.array([('abc',), ('de',)], dtype=[('f0', '<U3')])
-
-        >>> sequence_to_array(["abc", "de"],array_type="numpy.array"
-        ...     )  # doctest: +NORMALIZE_WHITESPACE
-        array([['abc'], ['de']], dtype='<U3')
-
-    """
-    if array_type == "numpy.array":
-        return _sequence_to_array(input)
-    if array_type == "numpy.rec.array":
-        return _sequence_to_recarray(input)
-    else:
-        raise NotImplementedError(f"{array_type=} not implemented")
-
-
-def _sequence_to_array(iterable):
-    """
-    Converts a finite sequence of experimental conditions into a 2D array.
+    Converts a finite sequence of experimental conditions into a 2D numpy.array.
 
     See also: [array_to_sequence][autora.experimentalist.utils.array_to_sequence]
 
@@ -174,9 +38,9 @@ def _sequence_to_array(iterable):
     return np.array(deque).reshape((len(deque), -1))
 
 
-def _sequence_to_recarray(iterable):
+def sequence_to_recarray(iterable):
     """
-    Converts a finite sequence of experimental conditions into a 2D array.
+    Converts a finite sequence of experimental conditions into a numpy recarray.
 
     See also: [array_to_sequence][autora.experimentalist.utils.array_to_sequence]
 
@@ -211,7 +75,9 @@ def array_to_sequence(input: Union[np.array, np.recarray]):
     """
     Convert an array of experimental conditions into an iterable of smaller arrays.
 
-    See also: [sequence_to_array][autora.experimentalist.utils.sequence_to_array]
+    See also:
+        - [sequence_to_array][autora.experimentalist.utils.sequence_to_array]
+        - [sequence_to_array][autora.experimentalist.utils.sequence_to_recarray]
 
     Examples:
 
