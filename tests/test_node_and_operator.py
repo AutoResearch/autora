f1c5d24e81e1d28a121482047701b68c3d1b9d7a
listar2000 <35262801+listar2000@users.noreply.github.com>
2023-01-16 14:30:13 -0600

    Add helper testing function

--- tests/test_node_and_operator.py
+++ tests/test_node_and_operator.py
@@ -1,15 +1,59 @@
-from autora_bsr.utils.node import Node, NodeType
-from autora_bsr.utils.operation import inv_op
+from autora_bsr.utils.node import Node
+from autora_bsr.utils.prior import get_prior_dict
+from typing import List, Union
 import numpy as np
 
 
-def test_basic_inv_operation():
+def __build_tree_from_literals(literals: List[Union[str, int]], **hyper_params):
+    """
+    Helper testing function that builds up a valid computation tree with a list of str/int inputs
+    where a string input represents an operation (e.g. `inv`, `+`) and an integer indicates which
+    feature to use in a leaf node. For the list of valid operations, see `priors.py`.
+
+    The construction is done level-by-level. For example, the list `["sin", "inv", 1, 0] will render
+    the following computation tree
+
+                sin (root)
+                /       \
+             inv      feature 1
+             /
+        feature 0
+
+    Note that for simplicity this function doesn't check the validity of input list; e.g. using
+    a binary operation without specifying the features used by its two leaf nodes might cause error.
+
+    Arguments:
+        literals: a list of strings or integers that specifies how the tree should be built
+        hyper_params: parameters to initialize certain operations
+    Returns:
+        root: the root node of the tree
+    """
+    _, _, prior_dict = get_prior_dict()
     root = Node(0)
-    root.operator = inv_op
-    root.type = NodeType.UNARY
-    root.left = Node(1)
-    root.left.type = NodeType.LEAF
-    root.left.feature = 0
+    node_queue = [root]
+    for s in literals:
+        node = node_queue.pop(0)
+        params = {}
+        if isinstance(s, str):
+            ops_init = prior_dict[s]["init"]
+            # init is a function randomized by some hyper-params
+            if callable(ops_init):
+                params.update(ops_init(**hyper_params))
+            else:  # init is deterministic dict
+                params.update(ops_init)
+            node.setup(s, prior_dict[s]["fn"], prior_dict[s]["arity"])
+        else:
+            params["feature"] = s
+            node.setup(**params)
+        if node.left:
+            node_queue.append(node.left)
+        if node.right:
+            node_queue.append(node.right)
+    return root
+
+
+def test_basic_inv_operation():
+    root = __build_tree_from_literals(["inv", 0])
     test_x = np.array([[1, 2, 3], [4, 5, 6]])
     test_y = 1 / test_x[:, 0]
     assert (test_y - root.evaluate(test_x) < 1e-5).all()
