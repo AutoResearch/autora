fb5909b3360d27157b7c659b42b611619d4a8664
Sebastian Musslick <sebastian@musslick.de>
2023-01-17 17:21:42 -0500

    updated slurm script

--- studies/cogsci2023/run_closed_loop_recovery.sh
+++ studies/cogsci2023/run_closed_loop_recovery.sh
@@ -1,9 +1,10 @@
-#SBATCH -J Experimentalists
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=1:00:00
-#SBATCH --array=1-10
+#!/bin/sh
 
+#SBATCH -J Experimentalists
+#SBATCH --time=1:00:00
+#SBATCH --array=1-20
+#SBATCH --mem=32GB
+#SBATCH -n 1
 
 # Print key runtime properties for records
 echo Master process running on `hostname`
@@ -15,7 +16,7 @@ module load graphviz/2.40.1
 module load python/3.9.0
 module load git/2.29.2
 source ~/autora_dev/bin/activate
-
+cd /users/smusslic/research/AER_repo/studies/cogsci2023
 
 # Run job
-python /users/smusslic/research/AER_repo/studies/cogsci2023/run_closed_loop_recovery_slurm.py SLURM_ARRAY_TASK_ID
+python run_closed_loop_recovery_slurm.py --slurm_id $SLURM_ARRAY_TASK_ID
