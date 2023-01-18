e2f11506bb08f92a3b9643c033d337f74ac4b2cf
Sebastian Musslick <sebastian@musslick.de>
2023-01-18 16:55:42 -0500

    updated slurm script

--- studies/cogsci2023/run_closed_loop_recovery_slurm.py
+++ studies/cogsci2023/run_closed_loop_recovery_slurm.py
@@ -20,10 +20,10 @@ args = parser.parse_args()
 rep = args.slurm_id
 
 # META PARAMETERS
-num_cycles = 200  # number of cycles (20)
-samples_for_seed = 1  # number of seed data_closed_loop points (20)
-samples_per_cycle = 1  # number of data_closed_loop points chosen per cycle (20)
-theorist_epochs = 500  # number of epochs for BMS (500)
+num_cycles = 20  # number of cycles (20)
+samples_for_seed = 10  # number of seed data_closed_loop points (20)
+samples_per_cycle = 10  # number of data_closed_loop points chosen per cycle (20)
+theorist_epochs = 1000  # number of epochs for BMS (500)
 
 # next stesp:
 # - up fitting to 3000 epochs (make sure to upp time running)
