45111f3bf9014154bde4be4fd5370f100b826298
Sebastian Musslick <sebastian@musslick.de>
2023-01-17 20:50:42 -0500

    updated slurm script

--- studies/cogsci2023/slurm_dummy.py
+++ studies/cogsci2023/slurm_dummy.py
@@ -1,1 +1,48 @@
 print("hello world")
+
+import pickle
+import time
+import argparse
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from models.models import model_inventory
+from utils import (
+    fit_theorist,
+    get_experimentalist,
+    get_MSE,
+    get_seed_experimentalist,
+)
+from run_closed_loop_recovery import closed_loop
+
+# parse arguments
+parser = argparse.ArgumentParser("parser")
+parser.add_argument('--slurm_id', type=int, default=0, help='number of slurm array')
+args = parser.parse_args()
+rep = args.slurm_id
+
+print("hello world2")
+
+# META PARAMETERS
+num_cycles = 20  # number of cycles (20)
+samples_for_seed = 10  # number of seed data_closed_loop points (20)
+samples_per_cycle = 10  # number of data_closed_loop points chosen per cycle (20)
+theorist_epochs = 1500  # number of epochs for BMS (500)
+
+# SELECT THEORIST
+# OPTIONS: BMS, DARTS
+theorist_name = "BMS"
+
+# SELECT GROUND TRUTH MODEL
+ground_truth_name = "prospect_theory"  # OPTIONS: see models.py
+
+experimentalists = [
+    # 'popper',
+    # 'falsification',
+    'random',
+    # "dissimilarity",
+    # 'model disagreement',
+    # 'least confident',
+]
+
+print("hello world3")
