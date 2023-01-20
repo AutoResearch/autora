7c555e2310032f2cd2cc5fe1f9849f53e00ae944
John Gerrard Holland <john_holland1@brown.edu>
2023-01-20 18:11:06 +0100

    test: add tests of registration and retrieval

--- tests/test_model_inventory.py
+++ tests/test_model_inventory.py
@@ -1,13 +1,30 @@
-import warnings
-
 import pytest  # noqa: 401
 
 import autora.synthetic
+from autora.variable import VariableCollection
 
-warnings.filterwarnings("ignore")
+
+def test_model_registration():
+    # We can register a model and retrieve it
+    autora.synthetic.register(id="empty")
+    empty = autora.synthetic.retrieve("empty")
+    assert empty.id == "empty"
+    assert empty.name is None
+
+    # We can register another model and retrieve it as well
+    autora.synthetic.register(
+        id="only_metadata", metadata_callable=lambda: VariableCollection()
+    )
+    only_metadata = autora.synthetic.retrieve("only_metadata")
+    assert only_metadata.id == "only_metadata"
+    assert only_metadata.metadata_callable()
+
+    # We can still retrieve the first model and it is equal to the first version
+    empty_copy = autora.synthetic.retrieve("empty")
+    assert empty_copy == empty
 
 
-def test_model_recovery():
+def test_model_retrieval():
     for id in ["weber_fechner", "expected_value", "prospect_theory"]:
         model = autora.synthetic.retrieve(id)
-        print(model)
+        assert model.id == id
