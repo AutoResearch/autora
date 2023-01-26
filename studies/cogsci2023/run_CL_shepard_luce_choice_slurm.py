9b9e31e26e0bd2c3ee243ef7873f06109fbc5a14
Sebastian Musslick <sebastian@musslick.de>
2023-01-26 09:41:18 -0500

    updated slurm script

--- studies/cogsci2023/run_CL_shepard_luce_choice_slurm.py
+++ studies/cogsci2023/run_CL_shepard_luce_choice_slurm.py
@@ -45,9 +45,9 @@ repetitions = np.arange(num_repetitions)
 experimentalist_ids = np.arange(len(experimentalists))
 conditions = np.array(np.meshgrid(repetitions, experimentalist_ids)).T.reshape(-1,2)
 
-ID = 2
-rep = conditions[ID,0] # args.slurm_id
-experimentalist_id = conditions[ID,1]
+# ID = 2
+rep = conditions[args.slurm_id,0] # args.slurm_id
+experimentalist_id = conditions[args.slurm_id,1]
 experimentalist_name = experimentalists[experimentalist_id]
 seed(rep)
 
