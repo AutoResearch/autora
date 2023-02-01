12232cab5e47feb9255970622e9502e0d9937444
John Gerrard Holland <john_holland1@brown.edu>
2023-02-01 09:46:02 -0500

    fix: id_ parameter name

--- tests/test_model_inventory.py
+++ tests/test_model_inventory.py
@@ -4,18 +4,16 @@ from autora.variable import VariableCollection
 
 def test_model_registration():
     # We can register a model and retrieve it
-    autora.synthetic.register(id="empty")
+    autora.synthetic.register("empty")
     empty = autora.synthetic.retrieve("empty")
-    assert empty.id == "empty"
+    assert empty.id_ == "empty"
     assert empty.name is None
 
     # We can register another model and retrieve it as well
-    autora.synthetic.register(
-        id="only_metadata", metadata_callable=lambda: VariableCollection()
-    )
+    autora.synthetic.register(id_="only_metadata", metadata=VariableCollection())
     only_metadata = autora.synthetic.retrieve("only_metadata")
-    assert only_metadata.id == "only_metadata"
-    assert only_metadata.metadata_callable()
+    assert only_metadata.id_ == "only_metadata"
+    assert only_metadata.metadata is not None
 
     # We can still retrieve the first model, and it is equal to the first version
     empty_copy = autora.synthetic.retrieve("empty")
