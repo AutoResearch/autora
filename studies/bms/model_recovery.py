6edaf1b0a64e70cae03076fa7e32030cf0663865
TheLemonPig <joshua_hewson@brown.edu>
2023-02-07 15:48:38 -0500

    second recoverable gt added

--- studies/bms/model_recovery.py
+++ studies/bms/model_recovery.py
@@ -4,39 +4,35 @@ import pandas as pd
 
 from autora.skl.bms import BMSRegressor
 from autora.skl.darts import DARTSRegressor
-from studies.bms.models.context_free.weber import weber
+from studies.bms.models.context_free.weber import weber_experiment
+from studies.bms.models.context_free.exp_learning import exp_learning_experiment
 
-# get domains
-weber_data = []
-x1_domain = range(1, 10)
-x2_domain = range(1, 10)
 
-# make data
-for x1 in x1_domain:
-    for x2 in x2_domain:
-        if x2 >= x1:
-            weber_data.append([x1, x2, weber(x1, x2)])
+# make & collect data
+data = dict()
+data['weber'] = weber_experiment()
+data['exp_learning'] = exp_learning_experiment()
 
-weber_data = pd.DataFrame(weber_data)
-print(weber_data.shape)
-bms = BMSRegressor(epochs=500)
-darts = DARTSRegressor(max_epochs=100)
-
-X = weber_data.iloc[:, :2]
-y = weber_data.iloc[:, -1:]
-
-bms.fit(X, y)
-darts.fit(X, y)
+# record theorist model recovery attempts
+recovery = dict()
+theorist = dict()
+bms = BMSRegressor(epochs=5)
+darts = DARTSRegressor(max_epochs=5)
+for key in data.keys():
+    X, y = data[key]
+    theorist['BMS'] = bms.fit(X, y).model_
+    theorist['DARTS'] = darts.fit(X, y).model_
+    recovery[key] = {
+        'data': data[key],
+        'theorist': theorist
+    }
 
 # save and load pickle file
 file_name = "data/data.pickle"
 
 with open(file_name, "wb") as f:
-    # simulation configuration
-    configuration = dict()
-    configuration["domain"] = x1_domain, x2_domain
-    configuration["data"] = X, y
-    object_list = [configuration]
+    # simulation recovery
+    object_list = [recovery]
 
     pickle.dump(object_list, f)
 
--- studies/bms/models/context_free/weber.py
+++ studies/bms/models/context_free/weber.py
@@ -1,4 +1,19 @@
 from numpy import log
+import numpy as np
+
+x1_domain = range(1, 10)
+x2_domain = range(1, 10)
+
+
+def weber_experiment():
+    X = []
+    y = []
+    for x1 in x1_domain:
+        for x2 in x2_domain:
+            if x2 >= x1:
+                X.append([x1, x2])
+                y.append(weber(x1, x2))
+    return np.array(X), np.array(y)
 
 
 def weber(x1: float, x2: float):
