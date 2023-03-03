93d8e9e2d5e96f801a3d799ba34397f5bd941efd
TheLemonPig <joshua_hewson@brown.edu>
2023-03-03 13:20:26 -0500

    update slurm file locally

--- studies/cogsci2023/run_theorist_recovery.sh
+++ studies/cogsci2023/run_theorist_recovery.sh
@@ -16,7 +16,7 @@ module load graphviz/2.40.1
 module load python/3.9.0
 module load git/2.29.2
 source ~/autora_dev/bin/activate
-cd /users/smusslic/research/AER_repo/studies/cogsci2023
+cd /users/jhewson/studies/cogsci2023
 
 # Run job
 python run_theorist_recovery_slurm.py --slurm_id $SLURM_ARRAY_TASK_ID
