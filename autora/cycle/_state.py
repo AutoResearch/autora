cda893c3016ee3b4942a82a4eea708bef01724d0
John Gerrard Holland <john_holland1@brown.edu>
2023-03-14 20:01:56 -0400

    docs: add a bunch of tests for the properties of the CycleState

--- autora/cycle/_state.py
+++ autora/cycle/_state.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Any, Dict, List, Optional, Protocol, Sequence
+from typing import Any, Dict, List, Optional, Protocol, Sequence, Union
 
 from numpy._typing import ArrayLike
 from sklearn.base import BaseEstimator
@@ -48,6 +48,12 @@ class CycleState:
         else:
             self.data = []
 
+        if metadata is not None:
+            self.metadata = metadata
+
+        if params is not None:
+            self.params = params
+
         for seq, kind in [
             (conditions, ResultKind.CONDITION),
             (observations, ResultKind.OBSERVATION),
@@ -57,17 +63,77 @@ class CycleState:
                 for i in seq:
                     self.update(i, kind=kind)
 
-        if metadata is not None:
-            self.metadata = metadata
+    def update(self, value, kind: Union[ResultKind, str]):
+        """
+        Add a new value.
 
-        if params is not None:
-            self.params = params
+        Examples:
+            First, create an empty state:
+            >>> state = CycleState()
 
-    def update(self, value, kind):
+            Now add data and any metadata required. The `kind` can be specified as a string:
+            >>> state.update("first metadata", kind="METADATA")
+            >>> state.metadata
+            'first metadata'
+
+            ... or by using the ResultKind enum. The effect is the same.
+            >>> state.update("second metadata", kind=ResultKind.METADATA)
+            >>> state.metadata
+            'second metadata'
+
+            >>> state.update("first theory", kind="THEORY")
+            >>> state.theories
+            ['first theory']
+
+            >>> state.update("first condition", kind="CONDITION")
+            >>> state.conditions
+            ['first condition']
+
+            >>> state.results  # doctest: +NORMALIZE_WHITESPACE
+            [Result(data='first theory', kind=ResultKind.THEORY),
+             Result(data='first condition', kind=ResultKind.CONDITION)]
+
+            >>> state.data  # doctest: +NORMALIZE_WHITESPACE
+            [Result(data='first metadata',  kind=ResultKind.METADATA),
+             Result(data='second metadata', kind=ResultKind.METADATA),
+             Result(data='first theory',    kind=ResultKind.THEORY),
+             Result(data='first condition', kind=ResultKind.CONDITION)]
+
+        """
         self.data.append(Result(value, ResultKind(kind)))
 
     @property
     def metadata(self) -> VariableCollection:
+        """
+        Access the newest metadata.
+
+        Examples:
+             >>> state = CycleState()
+
+             If we ask for a value which isn't set, then we get a default empty VariableCollection:
+             >>> state.metadata
+             VariableCollection(independent_variables=[], dependent_variables=[], covariates=[])
+
+             We can add a new metadata object by using the setter:
+             >>> from autora.variable import IV
+             >>> state.metadata = VariableCollection([IV(name="IV1")])
+             >>> state.metadata  # doctest: +ELLIPSIS
+             VariableCollection(independent_variables=[IV(name='IV1', ...)], ...)
+
+             ... or by calling "update" directly:
+             >>> state.update("new metadata", kind=ResultKind.METADATA)
+
+             That adds the metadata object to the .data attribute:
+             >>> state.data  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+             [Result(data=VariableCollection(independent_variables=[IV(name='IV1',...),
+                     kind=ResultKind.METADATA),
+              Result(data='new metadata', kind=ResultKind.METADATA)]
+
+              If we access the metadata property, we just get the newest one back:
+              >>> state.metadata
+              'new metadata'
+
+        """
         try:
             m = self._get_last(kind={ResultKind.METADATA}).data
         except StopIteration:
@@ -80,6 +146,34 @@ class CycleState:
 
     @property
     def params(self) -> Dict:
+        """
+        Access the newest params.
+
+        Examples:
+             >>> state = CycleState()
+
+             If we ask for a value which isn't set, then we get a default empty dictionary:
+             >>> state.params
+             {}
+
+             We can add a new params object by using the setter:
+             >>> state.params = {"theorist": {"n_epochs": 111}}
+             >>> state.params  # doctest: +ELLIPSIS
+             {'theorist': {'n_epochs': 111}}
+
+             ... or by calling "update" directly:
+             >>> state.update({"theorist": {"n_epochs": 222}}, kind=ResultKind.PARAMS)
+
+             That adds the metadata object to the .data attribute:
+             >>> state.data  # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
+             [Result(data={'theorist': {'n_epochs': 111}}, kind=ResultKind.PARAMS),
+              Result(data={'theorist': {'n_epochs': 222}}, kind=ResultKind.PARAMS)]
+
+              If we access the params property, we get the second one back:
+              >>> state.params
+              {'theorist': {'n_epochs': 222}}
+
+        """
         try:
             p = self._get_last(kind={ResultKind.PARAMS}).data
         except StopIteration:
@@ -92,22 +186,131 @@ class CycleState:
 
     @property
     def conditions(self) -> List[ArrayLike]:
+        """
+        Get all the results of kind "CONDITION"
+
+        Examples:
+            Initially, we get an empty list back:
+            >>> state = CycleState()
+            >>> state.conditions
+            []
+
+            We can add new conditions by using the `.update` method and specifying the kind as
+            `"CONDITION"`:
+            >>> import numpy as np
+            >>> state.update(np.array([11,12,13]), kind="CONDITION")
+            >>> state.conditions
+            [array([11, 12, 13])]
+
+            When we add multiple conditions, we get them all back:
+            >>> state.update(np.array([21,22,23]), kind="CONDITION")
+            >>> state.update(np.array([31,32,33]), kind="CONDITION")
+            >>> state.conditions
+            [array([11, 12, 13]), array([21, 22, 23]), array([31, 32, 33])]
+
+        """
         return self._list_data(
             self._filter_result(self.data, kind={ResultKind.CONDITION})
         )
 
     @property
     def observations(self) -> List[ArrayLike]:
+        """
+        Get all the results of kind "OBSERVATION"
+
+        Examples:
+            Initially, we get an empty list back:
+            >>> state = CycleState()
+            >>> state.observations
+            []
+
+            We can add new conditions by using the `.update` method and specifying the kind as
+            `"OBSERVATION"`:
+            >>> import numpy as np
+            >>> state.update(np.array([11,12,13]), kind="OBSERVATION")
+            >>> state.observations
+            [array([11, 12, 13])]
+
+            When we add multiple conditions, we get them all back:
+            >>> state.update(np.array([21,22,23]), kind="OBSERVATION")
+            >>> state.update(np.array([31,32,33]), kind="OBSERVATION")
+            >>> state.observations
+            [array([11, 12, 13]), array([21, 22, 23]), array([31, 32, 33])]
+
+        """
         return self._list_data(
             self._filter_result(self.data, kind={ResultKind.OBSERVATION})
         )
 
     @property
     def theories(self) -> List[BaseEstimator]:
+        """
+        Get all the results of kind "THEORY"
+
+        Examples:
+            Initially, we get an empty list back:
+            >>> state = CycleState()
+            >>> state.theories
+            []
+
+            We can add new conditions by using the `.update` method and specifying the kind as
+            `"THEORY"`:
+            >>> from sklearn.linear_model import LinearRegression
+            >>> import numpy as np
+            >>> theory_1 = LinearRegression().fit(np.array([[11,12,13]]), np.array([[111,112,113]]))
+            >>> state.update(theory_1, kind="THEORY")
+            >>> state.theories
+            [LinearRegression()]
+
+            When we add multiple theories, we get them all back:
+            >>> theory_2 = LinearRegression().fit(np.array([[21,22,23]]), np.array([[121,122,123]]))
+            >>> state.update(theory_2, kind="THEORY")
+            >>> state.theories
+            [LinearRegression(), LinearRegression()]
+        """
         return self._list_data(self._filter_result(self.data, kind={ResultKind.THEORY}))
 
     @property
     def results(self) -> List[Result]:
+        """
+        Get all the CONDITION, OBSERVATION and THEORY result objects
+
+        Examples:
+            First, we create an empty state:
+            >>> state = CycleState()
+
+            Initially, `.results` is empty:
+            >>> state.results
+            []
+
+            Usually there will be a metadata entry:
+            >>> state.update("0 - metadata", kind="METADATA")
+
+            We add a series of results in order:
+            >>> state.update("1 - theory", kind="THEORY")
+            >>> state.update("2 - condition", kind="CONDITION")
+            >>> state.update("3 - observation", kind="OBSERVATION")
+            >>> state.update("4 - theory", kind="THEORY")
+            >>> state.update("5 - theory", kind="THEORY")
+            >>> state.update("6 - observation", kind="THEORY")
+
+            `.results` now includes all the results excluding the metadata:
+            >>> state.results  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+            [Result(data='1 - theory', kind=ResultKind.THEORY),
+             Result(data='2 - condition', kind=ResultKind.CONDITION),
+             Result(data='3 - observation', kind=ResultKind.OBSERVATION),
+             Result(data='4 - theory', kind=ResultKind.THEORY),
+             Result(data='5 - theory', kind=ResultKind.THEORY),
+             Result(data='6 - observation', kind=ResultKind.THEORY)]
+
+            If we add a PARAMS object, this is also ignored:
+            >>> state.update({"some": "params"}, kind="PARAMS")
+            >>> state.results  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+            [Result(data='1 - theory', kind=ResultKind.THEORY),
+            ...
+            Result(data='6 - observation', kind=ResultKind.THEORY)]
+
+        """
         return list(
             self._filter_result(
                 self.data,
