952e8197739cfd8710052e3f1acced6a09d7baf2
John Gerrard Holland <john_holland1@brown.edu>
2023-03-17 09:26:13 -0400

    refactor: remove protected members from export

--- autora/cycle/state/__init__.py
+++ autora/cycle/state/__init__.py
@@ -1,12 +1,3 @@
-from .history import (
-    Result,
-    SimpleCycleDataHistory,
-    _filter_history,
-    _get_last,
-    _get_last_data_with_default,
-    _history_to_kind,
-    _init_result_list,
-    _list_data,
-)
+from .history import Result, SimpleCycleDataHistory
 from .param import resolve_state_params
 from .simple import SimpleCycleData
