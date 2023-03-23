a907ca7c00c6a5a3fe5883b112fc1da9d58e996d
Sebastian Musslick <sebastian@musslick.de>
2023-03-23 13:20:29 -0400

    added tests to falsification sampler

--- autora/experimentalist/sampler/__init__.py
+++ autora/experimentalist/sampler/__init__.py
@@ -1,4 +1,5 @@
 from .assumption import assumption_sampler
+from .falsification import falsification_sampler
 from .model_disagreement import model_disagreement_sampler
 from .nearest_value import nearest_values_sampler
 from .random import random_sampler
