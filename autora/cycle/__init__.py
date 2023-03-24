dc9d118f6a80554ab27da0a37c371c76b5ca6465
John Gerrard Holland <john_holland1@brown.edu>
2023-03-24 08:39:23 -0400

    refactor: change imports in autora/cycle to import from controller module, and include deprecation warning

--- autora/cycle/__init__.py
+++ autora/cycle/__init__.py
@@ -1,8 +1,17 @@
-from .plot_utils import (
+from warnings import warn
+
+from ..controller.cycle import Cycle
+from ..controller.plotting import (
     cycle_default_score,
     cycle_specified_score,
     plot_cycle_score,
     plot_results_panel_2d,
     plot_results_panel_3d,
 )
-from .simple import SimpleCycle as Cycle
+
+warn(
+    "The `autora.cycle` module is deprecated. "
+    "Use the new `autora.controller` module",
+    DeprecationWarning,
+    stacklevel=2,
+)
