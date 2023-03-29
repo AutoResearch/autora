47394d08e9a3d6f78688f378d8057a00cce24bb3
John Gerrard Holland <john_holland1@brown.edu>
2023-03-29 15:29:39 -0400

    refactor: working subprocess dispatch which waits

--- autora/controller/executor/_subprocess.py
+++ autora/controller/executor/_subprocess.py
@@ -1,10 +1,10 @@
 import asyncio
 import pathlib
-import pickle
 import sys
 import tempfile
-import time
 from typing import TypeVar
+
+import dill
 
 Controller_ = TypeVar("Controller_")
 
@@ -20,49 +20,49 @@ async def dispatch_to_subprocess(controller: Controller_) -> Controller_:
 
     Examples:
         >>> from autora.controller import Controller
-        >>> from
-        >>> c = Controller(metadata=None, theories=[""], experiment_runner=)
-        >>> cn = asyncio.run(dispatch_to_subprocess(c))
-        <Process ...>
-        >>> cn
+        >>> import numpy as np
+        >>> def plus_1(x):
+        ...     return x + 1
+        >>> c = Controller(metadata=None, experiment_runner=plus_1)
+        >>> c.state = c.state.update(conditions=[np.array([1,2,3])])
+        >>> cn = asyncio.run(dispatch_to_subprocess(c))  # doctest: +ELLIPSIS
+        >>> cn  # doctest: +ELLIPSIS
         <autora...Controller object at 0x...>
+
+        >>> cn.state  # doctest: +NORMALIZE_WHITESPACE
+        History([Result(data={}, kind=ResultKind.PARAMS),
+        Result(data=array([1, 2, 3]), kind=ResultKind.CONDITION),
+        Result(data=array([[1, 2],
+                           [2, 3],
+                           [3, 4]]), kind=ResultKind.OBSERVATION)])
 
     """
     with tempfile.TemporaryDirectory() as tmpdir:
 
-        # Testing code --------------
-        import subprocess
-
-        subprocess.call(["open", "-a", "Finder", tmpdir])
-        # ---------------------------
-
-        input_filepath = pathlib.Path(tmpdir, "function.pickle")
-        output_filepath = pathlib.Path(tmpdir, "out.pickle")
+        input_filepath = pathlib.Path(tmpdir, "function.dill")
+        output_filepath = pathlib.Path(tmpdir, "out.dill")
         with open(input_filepath, "wb") as input_file:
-            pickle.dump(controller, input_file)
+            dill.dump(controller, input_file)
 
         code = f"""
-import pickle
-import time
+import dill
 
 with open("{input_filepath}", 'rb') as input_file:
-    controller = pickle.load(input_file)
+    controller = dill.load(input_file)
 
 updated_controller = next(controller)
 
 with open("{output_filepath}", 'wb') as output_file:
-    pickle.dump(updated_controller, output_file)
+    dill.dump(updated_controller, output_file)
 
 """
         proc = await asyncio.create_subprocess_exec(
             sys.executable, "-c", code, stdout=asyncio.subprocess.PIPE
         )
 
-        print(proc)
-
-        time.sleep(5)
+        await proc.wait()
 
         with open(output_filepath, "rb") as output_file:
-            result = pickle.load(output_file)
+            result = dill.load(output_file)
 
     return result
