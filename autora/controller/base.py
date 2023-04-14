d0f96d6a19e3d7088c4c7140d9006a155cd6bab1
John Gerrard Holland <john_holland1@brown.edu>
2023-04-14 09:19:38 -0400

    revert: remove run_fn

--- autora/controller/base.py
+++ autora/controller/base.py
@@ -84,10 +84,3 @@ class BaseController:
 
     def __iter__(self):
         return self
-
-    def run_fn(self, function_name: ExecutorName):
-        initial_planner = self.planner
-        self.planner = lambda s: function_name
-        next(self)
-        self.planner = initial_planner
-        return self
