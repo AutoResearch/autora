e61f73b9e9af23fa84e1e31ab3ad49ae4b0393ad
John Gerrard Holland <john_holland1@brown.edu>
2023-04-21 15:51:20 -0400

    Revert "docs: update mkdocs settings"
    This reverts commit cf0784e97d99b4275dd0ba8b769cd955c36b454f.

--- mkdocs/_gen_ref_pages.py
+++ mkdocs/_gen_ref_pages.py
@@ -1,35 +1,36 @@
 """Generate the code reference pages and navigation."""
 
 from pathlib import Path
+from typing import List
 
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-namespace_src_dirs = sorted(Path("./temp_dir").rglob("**/src/"))
-for src_dir in namespace_src_dirs:
-    source_paths = sorted(src_dir.rglob("*.py"))
-    for path in source_paths:
-        module_path = path.relative_to(src_dir).with_suffix("")
-        doc_path = path.relative_to(".").with_suffix(".md")
-        full_doc_path = Path("reference", doc_path)
+source_paths = sorted(Path("./autora").rglob("*.py"))
+special_paths: List[Path] = []
 
-        parts = tuple(module_path.parts)
+for path in source_paths + special_paths:
+    module_path = path.relative_to(".").with_suffix("")
+    doc_path = path.relative_to(".").with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
 
-        if parts[-1] == "__init__":
-            parts = parts[:-1]
-            doc_path = doc_path.with_name("index.md")
-            full_doc_path = full_doc_path.with_name("index.md")
-        elif parts[-1] == "__main__":
-            continue
+    parts = tuple(module_path.parts)
 
-        nav[parts] = doc_path.as_posix()
+    if parts[-1] == "__init__":
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1] == "__main__":
+        continue
 
-        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-            ident = ".".join(parts)
-            fd.write(f"::: {ident}")
+    nav[parts] = doc_path.as_posix()
 
-        mkdocs_gen_files.set_edit_path(full_doc_path, path)
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        ident = ".".join(parts)
+        fd.write(f"::: {ident}")
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
