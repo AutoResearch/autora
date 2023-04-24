329bb4c6975f94dc9d17e52ef96d8e9f3b134d7e
Sebastian Musslick <sebastian@musslick.de>
2023-04-24 19:15:40 -0400

    optimized distance calculation and added score

--- autora/experimentalist/sampler/dissimilarity.py
+++ autora/experimentalist/sampler/dissimilarity.py
@@ -2,6 +2,7 @@ from typing import Iterable, Literal
 
 import numpy as np
 from sklearn.metrics import DistanceMetric
+from sklearn.preprocessing import StandardScaler
 
 AllowedMetrics = Literal[
     "euclidean",
@@ -28,7 +29,11 @@ AllowedMetrics = Literal[
 
 
 def summed_dissimilarity_sampler(
-    X: np.ndarray, X_ref: np.ndarray, n: int = 1, metric: AllowedMetrics = "euclidean"
+    X: np.ndarray,
+    X_ref: np.ndarray,
+    n: int = 1,
+    metric: AllowedMetrics = "euclidean",
+    integration: str = "sum",
 ) -> np.ndarray:
     """
     This dissimilarity samples re-arranges the pool of IV conditions according to their
@@ -44,6 +49,39 @@ def summed_dissimilarity_sampler(
             'hamming', 'canberra', 'braycurtis', 'matching', 'jaccard', 'dice',
             'kulsinski', 'rogerstanimoto', 'russellrao', 'sokalmichener',
             'sokalsneath', 'yule'. See [sklearn.metrics.DistanceMetric][] for more details.
+
+    Returns:
+        Sampled pool
+    """
+
+    new_X, distance_scores = compute_dissimilarity(X, X_ref, n, metric, integration)
+
+    return new_X
+
+
+def compute_dissimilarity(
+    X: np.ndarray,
+    X_ref: np.ndarray,
+    n: int = 1,
+    metric: AllowedMetrics = "euclidean",
+    integration: str = "sum",
+) -> np.ndarray:
+    """
+    This dissimilarity samples re-arranges the pool of IV conditions according to their
+    dissimilarity with respect to a reference pool X_ref. The default dissimilarity is calculated
+    as the average of the pairwise distances between the conditions in X and X_ref.
+
+    Args:
+        X: pool of IV conditions to evaluate dissimilarity
+        X_ref: reference pool of IV conditions
+        n: number of samples to select
+        metric (str): dissimilarity measure. Options: 'euclidean', 'manhattan', 'chebyshev',
+            'minkowski', 'wminkowski', 'seuclidean', 'mahalanobis', 'haversine',
+            'hamming', 'canberra', 'braycurtis', 'matching', 'jaccard', 'dice',
+            'kulsinski', 'rogerstanimoto', 'russellrao', 'sokalmichener',
+            'sokalsneath', 'yule'. See [sklearn.metrics.DistanceMetric][] for more details.
+        integration: Distance integration method used to compute the overall dissimilarity score
+        for a given data point. Options: 'sum', 'prod', 'mean', 'min', 'max'.
 
     Returns:
         Sampled pool
@@ -74,23 +112,27 @@ def summed_dissimilarity_sampler(
 
     dist = DistanceMetric.get_metric(metric)
 
-    # create a list to store the summed distances for each row in matrix1
-    summed_distances = []
+    distances = dist.pairwise(X_ref, X)
 
-    # loop over each row in first matrix
-    for row in X:
-        # calculate the distances between the current row in matrix1 and all other rows in matrix2
-        summed_distance = 0
+    if integration == "sum":
+        integrated_distance = np.sum(distances, axis=0)
+    elif integration == "mean":
+        integrated_distance = np.mean(distances, axis=0)
+    elif integration == "max":
+        integrated_distance = np.max(distances, axis=0)
+    elif integration == "min":
+        integrated_distance = np.min(distances, axis=0)
+    elif integration == "prod":
+        integrated_distance = np.prod(distances, axis=0)
+    else:
+        raise ValueError(f"Integration method {integration} not supported.")
 
-        for X_ref_row in X_ref:
+    # normalize the distances
+    scaler = StandardScaler()
+    score = scaler.fit_transform(integrated_distance.reshape(-1, 1)).flatten()
 
-            distance = dist.pairwise([row, X_ref_row])[0, 1]
-            summed_distance += distance
+    # order rows in Y from highest to lowest
+    sorted_X = X[np.argsort(integrated_distance)[::-1]]
+    sorted_score = score[np.argsort(score)[::-1]]
 
-        # store the summed distance for the current row
-        summed_distances.append(summed_distance)
-
-    # sort the rows in matrix1 by their summed distances
-    sorted_X = X[np.argsort(summed_distances)[::-1]]
-
-    return sorted_X[:n]
+    return sorted_X[:n], sorted_score[:n]
