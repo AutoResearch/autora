a60f8d00778d676bad6b16e20e7d1345ed346091
John Gerrard Holland <john_holland1@brown.edu>
2023-04-26 13:02:44 -0400

    chore: update mathjax.js for arithmatex

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,16 +1,12 @@
 window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
-
-document$.subscribe(() => {
-  MathJax.typesetPromise()
-})
+    tex: {
+      inlineMath: [ ["\\(","\\)"] ],
+      displayMath: [ ["\\[","\\]"] ],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*",
+      processHtmlClass: "arithmatex"
+    }
+    };
