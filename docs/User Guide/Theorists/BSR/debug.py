7372cc156a60ad07819b0b441660d82d043556d5
listar2000 <35262801+listar2000@users.noreply.github.com>
2023-05-01 12:46:25 -0500

    Remove unnecessary files

--- docs/User Guide/Theorists/BSR/debug.py
+++ docs/User Guide/Theorists/BSR/debug.py
@@ -5,13 +5,13 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-X = np.random.uniform(-3, 3, (1000, 1))
+X = np.random.uniform(-3, 3, (1000, 2))
 # y = np.sin(X[:, 0]) + np.cos(X[:, 1])
-y = np.cos(X[:, 0]) + X[:, 0] ** 3
+y = np.cos(X[:, 0]) + X[:, 1] ** 3
 
 bsr_estimator = BSRRegressor(
     tree_num=3,
-    itr_num=100,
+    itr_num=200,
     val=100,
     beta=-1,
     show_log=True
@@ -19,15 +19,3 @@ bsr_estimator = BSRRegressor(
 
 bsr_estimator.fit(X, y)
 pred_y = bsr_estimator.predict(X)
-
-show = False
-if show:
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:,0], X[:,1], pred_y)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:,0], X[:,1], y)
-
-    plt.show()
