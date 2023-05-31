509dab2725eff744faa90ca0616c0412e0a3d017
John Gerrard Holland <john_holland1@brown.edu>
2023-05-31 12:37:52 -0400

    ci: add testcases

---
+++ tests/test_core_imports.py
@@ -0,0 +1,5 @@
+def test_core_imports():
+    # Only include default packages installed by `pip install autora` without any extras here
+    import autora  # noqa
+    import autora.variable  # noqa
+    import autora.workflow  # noqa
