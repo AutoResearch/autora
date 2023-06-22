b0da3265e855556dc8b515570c837ee2427b590f
John Gerrard Holland <john_holland1@brown.edu>
2023-06-22 10:53:53 -0400

    refactor: move function out of loop

--- mkdocs/generate_code_reference.py
+++ mkdocs/generate_code_reference.py
@@ -16,6 +16,52 @@ def source_file_generator(src_paths):
             yield path, src_path
 
 
+def get_reference_file_string(p):
+    """
+
+    Args:
+        p: a tuple of module names which combine to make an identifier
+
+    Returns:
+        A markdown string with the full combined module name (excluding any __init__ part) as
+        the L1-title and the mkdocstrings "::: <identifier>" below
+
+    Examples:
+        >>> print(get_reference_file_string(("a",)))
+        # a
+        <BLANKLINE>
+        ::: a
+
+        >>> print(get_reference_file_string(("__init__",)))
+        Traceback (most recent call last):
+        ...
+        AssertionError: __init__ must have a parent
+
+        >>> print(get_reference_file_string(("a", "b")))
+        # a.b
+        <BLANKLINE>
+        ::: a.b
+
+        >>> print(get_reference_file_string(("a", "b", "__init__")))
+        # a.b
+        <BLANKLINE>
+        ::: a.b.__init__
+
+                    >>> print(get_reference_file_string(("a", "b")))
+        # a.b
+        <BLANKLINE>
+        ::: a.b
+    """
+    if p[-1] == "__init__":
+        ident = ".".join(p[:-1])
+        assert ident != "", "__init__ must have a parent"
+    else:
+        ident = ".".join(p)
+    title = ident.replace("_", "\\_")
+    s = f"# {title}\n\n::: {ident}"
+    return s
+
+
 for path, src_path in source_file_generator(src_paths):
     module_path = path.relative_to(src_path).with_suffix("")
     doc_path = path.relative_to(src_path).with_suffix(".md")
@@ -28,51 +74,6 @@ for path, src_path in source_file_generator(src_paths):
         full_doc_path = full_doc_path.with_name("index.md")
 
     nav[parts] = doc_path.as_posix()
-
-    def get_reference_file_string(p):
-        """
-
-        Args:
-            p: a tuple of module names which combine to make an identifier
-
-        Returns:
-            A markdown string with the full combined module name (excluding any __init__ part) as
-            the L1-title and the mkdocstrings "::: <identifier>" below
-
-        Examples:
-            >>> print(get_reference_file_string(("a",)))
-            # a
-            <BLANKLINE>
-            ::: a
-
-            >>> print(get_reference_file_string(("__init__",)))
-            Traceback (most recent call last):
-            ...
-            AssertionError: __init__ must have a parent
-
-            >>> print(get_reference_file_string(("a", "b")))
-            # a.b
-            <BLANKLINE>
-            ::: a.b
-
-            >>> print(get_reference_file_string(("a", "b", "__init__")))
-            # a.b
-            <BLANKLINE>
-            ::: a.b.__init__
-
-                        >>> print(get_reference_file_string(("a", "b")))
-            # a.b
-            <BLANKLINE>
-            ::: a.b
-        """
-        if p[-1] == "__init__":
-            ident = ".".join(p[:-1])
-            assert ident != "", "__init__ must have a parent"
-        else:
-            ident = ".".join(p)
-        title = ident.replace("_", "\\_")
-        s = f"# {title}\n\n::: {ident}"
-        return s
 
     docstring_stub = get_reference_file_string(parts)
 
