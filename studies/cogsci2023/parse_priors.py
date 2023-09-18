196d29b793f62c1735bd37967bc81b7c00ec4057
TheLemonPig <joshua_hewson@brown.edu>
2023-09-18 12:30:36 -0400

    Add parse priors

---
+++ studies/cogsci2023/parse_priors.py
@@ -0,0 +1,23 @@
+import pickle
+
+priors_list = ['priors_SUPERCognitiveScience.pkl','priors_SUPERCognitivePsychology.pkl','priors_SUPERMaterialsScience.pkl','priors_SUPERNeuroscience.pkl']
+dfs = []
+for prior in priors_list:
+    with open('studies/cogsci2023/priors/'+prior, "rb") as f:
+        dfs.append(pickle.load(f))
+ops = set()
+total_equations = 0
+for df in dfs:
+    ops = ops.union(set(df['priors']['operators_and_functions'].keys()))
+ops = dict({op: 0 for op in ops})
+print(ops)
+for df in dfs:
+    prs = df['priors']['operators_and_functions']
+    print('\nNext Prior\n')
+    for pr in prs.items():
+        print(f"{pr[0]}: {pr[1]/df['metadata']['number_of_equations']}")
+        total_equations += df['metadata']['number_of_equations']
+        ops.update({pr[0]: ops[pr[0]]+pr[1]})
+    print('\n')
+for op in ops.items():
+    print(f"'{op[0]}': {op[1]/total_equations},")
